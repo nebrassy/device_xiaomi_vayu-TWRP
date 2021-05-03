b9f06d0e42448f7c1d312b3f2250ba81ddd62875
nebrassy <nebras30@gmail.com>
2021-05-03 19:33:07 +0200

    add fastbootd

--- device.mk
+++ device.mk
@@ -23,7 +23,9 @@ PRODUCT_SOONG_NAMESPACES += \
 
 PRODUCT_PACKAGES += \
     qcom_decrypt \
-    qcom_decrypt_fbe
+    qcom_decrypt_fbe \
+    fastbootd \
+    android.hardware.fastboot@1.0-impl-mock
 
 PRODUCT_SHIPPING_API_LEVEL := 30
 
