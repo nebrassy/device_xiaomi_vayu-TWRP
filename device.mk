eaf8219b66671e43f792c931c09cc59842caba0b
nebrassy <nebras30@gmail.com>
2022-06-20 10:31:26 +0200

    vayu: override some props from system
    Change-Id: Ib1128ca61556a803e5ff463629c58a05060466af

--- device.mk
+++ device.mk
@@ -68,6 +68,10 @@ TARGET_USES_MKE2FS := true
 TW_NO_SCREEN_BLANK := true
 TW_EXCLUDE_APEX := true
 
+#Properties
+TW_OVERRIDE_SYSTEM_PROPS := \
+    "ro.build.date.utc;ro.build.fingerprint=ro.system.build.fingerprint;ro.build.version.incremental"
+
 # Vibrator
 TW_SUPPORT_INPUT_AIDL_HAPTICS := true
 
